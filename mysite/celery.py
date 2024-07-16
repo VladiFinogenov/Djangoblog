dd12918fee8f59214ebe17b69183e21dd67b170f
VladiFinogenov <125090995+VladiFinogenov@users.noreply.github.com>
2024-07-16 23:17:42 +0300

    Update celery.py

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,16 +1,3 @@
-# import os
-# from celery import Celery
-# from django.conf import settings
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-# app = Celery('mysite', broker_connection_retry=False,
-#              broker_connection_retry_on_startup=True, )
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-# broker_connection_retry = False
-#
-# app.autodiscover_tasks()
-
-
 import os
 from celery import Celery
 
